76c0538889c036c02a9bb468c96f1f91ae6979c1
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-10-31 10:53:49 -0500

    Add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -87,6 +87,8 @@ html_theme_options = {
     'bootswatch_theme': 'flatly',
 }
 
+html_favicon = "images/favicon.ico"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
