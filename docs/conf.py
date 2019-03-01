3ce7eb16450cb7169a01cb198073f635cfd58735
Joshua McMichael <josh@jmcmichael.com>
2019-03-01 11:43:31 -0600

    switched theme to sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -80,22 +80,13 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bootstrap'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'bootswatch_theme': 'cosmo',
-
-    'navbar_title': "Home",
-    'navbar_site_name': 'Menu',
-    'source_link_position': False,
-
-    'navbar_sidebarrel': False,
-
-    'navbar_pagenav': False,
 }
 
 html_favicon = "images/favicon.ico"
