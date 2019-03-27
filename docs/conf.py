7d4b9b75a872bdb373d0cb9ca438f2b0ffaf028c
Joshua McMichael <josh@jmcmichael.com>
2019-03-27 16:35:39 -0500

    switching from pip rtd theme to locally stored theme to better control templates

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme_path = ["_themes", ]
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
