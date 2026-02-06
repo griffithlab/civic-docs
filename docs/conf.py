0b68b39a2b71858e2f8a87c6c4bad400306654b7
Susanna Kiwala <susanna.kiwala@wustl.edu>
2026-02-06 11:00:20 -0600

    Try a different latex engine

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,7 @@ htmlhelp_basename = 'CIViCdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
+latex_engine='xelatex'
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
